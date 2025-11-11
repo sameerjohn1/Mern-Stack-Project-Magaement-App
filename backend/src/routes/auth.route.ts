62cba6a2aa93ca394ebdbbba1dad2f18b09a241e
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-11-11 10:27:49 +0500

    Added user login  route with validation and tested successfully

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import passport from "passport";
 import { config } from "../config/app.config";
-import { googleLoginCallback, registerUserController } from "../controllers/auth.controller";
-// import { googleLoginCallback } from "../controllers/auth.controller";
+import { googleLoginCallback, loginController, registerUserController } from "../controllers/auth.controller";
 
 const failedUrl=`${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`
 
 const authRoutes=Router();
 
 authRoutes.post("/register",registerUserController)
+authRoutes.post("/login",loginController)
+
 
 authRoutes.get("/google",
     passport.authenticate("google",{
