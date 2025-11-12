b5eb6a1c5930b63d0e85de4ecc0ed08a7689908e
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-11-12 11:58:25 +0500

    add POST API to create workspace

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,7 @@ import passport from "passport";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./routes/workspace.route";
 
 const app=express();
 const BASE_PATH=config.BASE_PATH;
@@ -54,6 +55,8 @@ app.get("/",asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
 
 app.use(`${BASE_PATH}/auth`,authRoutes);
 app.use(`${BASE_PATH}/user`,isAuthenticated , userRoutes);
+app.use(`${BASE_PATH}/workspace`,isAuthenticated , workspaceRoutes);
+
 
 
 app.use(errorHandler)
---
+++ backend/src/routes/workspace.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createWorkspaceController 
+} from "../controllers/workspace.controller";
+
+const workspaceRoutes=Router();
+
+workspaceRoutes.post("/create/new", createWorkspaceController);
+
+
+export default workspaceRoutes
